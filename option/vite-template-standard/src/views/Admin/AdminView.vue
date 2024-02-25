eb63d0f15e057180f86b50a60de360f3932c1079
Hsuan <welcome09210801@gmail.com>
2024-02-26 07:28:09 +0800

    feat ( option api ) 新增登入登出

--- option/vite-template-standard/src/views/Admin/AdminView.vue
+++ option/vite-template-standard/src/views/Admin/AdminView.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="home">
     <h1>後台</h1>
-    <RouterLink to="/admin">後台首頁</RouterLink> |
-    <RouterLink to="/admin/product">產品</RouterLink> |
-    <RouterLink to="/admin/order">訂單</RouterLink> |
-    <RouterLink to="/admin/cupon">優惠券</RouterLink> |
-    <RouterLink to="/admin/post">貼文</RouterLink> |
-    <RouterLink to="/">登出</RouterLink> |
-    <RouterLink to="/admin/blog">Blog</RouterLink> |
-    <RouterLink to="/admin/cart">購物車</RouterLink> |
-  </div>
+    <!-- <div class="collapse navbar-collapse"> -->
+      <div class="navbar-nav">
+        <RouterLink to="/admin">後台首頁</RouterLink> |
+        <RouterLink to="/admin/product">產品</RouterLink> |
+        <RouterLink to="/admin/order">訂單</RouterLink> |
+        <RouterLink to="/admin/cupon">優惠券</RouterLink> |
+        <RouterLink to="/admin/post">貼文</RouterLink>
+        <a href="#" @click.prevent="logout" class="nav-link">登出</a>
+      </div>
+      <div class="navbar-nav ms-auto">
+        <RouterLink to="/admin/blog">Blog</RouterLink> |
+        <RouterLink to="/admin/cart">購物車</RouterLink>
+      </div>
+    </div>
+  <!-- </div> -->
   <hr />
   <RouterView></RouterView>
 </template>
 
-<script></script>
+<script>
+import axios from 'axios'
+const { VITE_API } = import.meta.env
+export default {
+  data () {
+    return {
+      url: VITE_API
+    }
+  },
+  methods: {
+    logout () {
+      axios
+        .post(`${this.url}/logout`)
+        // 成功的結果
+        .then((res) => {
+          console.log(res)
+          alert('登出成功')
+          this.$router.push('/')
+        })
+        // 失敗結果
+        .catch((error) => {
+          console.dir(error) // 用dir可以展開資訊
+        })
+    }
+  }
+}
+</script>
 
 <style scoped></style>
--- option/vite-template-standard/src/views/LoginView.vue
+++ option/vite-template-standard/src/views/LoginView.vue
@@ -1,7 +1,81 @@
 <template>
-  <h2>請先登入</h2>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+      <div class="col-8">
+        <form id="form" class="form-signin" @submit.prevent="submitLogin">
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="username"
+              placeholder="name@example.com"
+              required
+              autofocus
+              v-model="user.username"
+            />
+            <label for="username"></label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="Password"
+              v-model="user.password"
+              required
+            />
+            <label for="password">Password</label>
+          </div>
+          <button class="btn btn-lg btn-primary w-100 mt-3" type="submit">
+            登入
+          </button>
+        </form>
+      </div>
+    </div>
+    <p class="mt-5 mb-3 text-muted">&copy; 2021~∞ - 六角學院</p>
+  </div>
 </template>
 
-<script></script>
+<script>
+import axios from 'axios'
+const { VITE_API } = import.meta.env
 
-<style scoped></style>
+export default {
+  data () {
+    return {
+      url: VITE_API,
+      path: 'hsuanin-vue2024',
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      axios
+        .post(`${this.url}/admin/signin`, this.user)
+        // 成功的結果
+        .then((res) => {
+          console.log(res)
+          // unix timestamp
+          // 解構
+          const { token, expired } = res.data // 解構
+          console.log(token, expired)
+          // window.location = 'products.html'
+          this.$router.push('/admin/product')
+          // document.cookie = `someCookieName = true; expires = Fri,31 Dec 9999 23:59 GMT;`;
+          document.cookie = `hexToken= ${token}; expires=${new Date(
+            expired
+          )}; path=/`
+        })
+        // 失敗結果
+        .catch((error) => {
+          console.dir(error) // 用dir可以展開資訊
+        })
+    }
+  },
+  mounted () {}
+}
+</script>
