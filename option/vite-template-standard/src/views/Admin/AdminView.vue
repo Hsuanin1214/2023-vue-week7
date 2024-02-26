139f0322d848834e8782fa2f99bc56846268960c
ERI\welcome09210801 <welcome09210801@eri.com.tw>
2024-02-26 17:57:33 +0800

    feat ( option api) 新增視窗modal及訂單

--- option/vite-template-standard/src/views/Admin/AdminView.vue
+++ option/vite-template-standard/src/views/Admin/AdminView.vue
@@ -4,10 +4,10 @@
     <!-- <div class="collapse navbar-collapse"> -->
       <div class="navbar-nav">
         <RouterLink to="/admin">後台首頁</RouterLink> |
-        <RouterLink to="/admin/product">產品</RouterLink> |
-        <RouterLink to="/admin/order">訂單</RouterLink> |
-        <RouterLink to="/admin/cupon">優惠券</RouterLink> |
-        <RouterLink to="/admin/post">貼文</RouterLink>
+        <RouterLink to="/admin/product">產品管理</RouterLink> |
+        <RouterLink to="/admin/order">訂單管理</RouterLink> |
+        <RouterLink to="/admin/cupon">優惠券管理</RouterLink> |
+        <RouterLink to="/admin/post">貼文管理</RouterLink>
         <a href="#" @click.prevent="logout" class="nav-link">登出</a>
       </div>
       <div class="navbar-nav ms-auto">
@@ -22,26 +22,27 @@
 
 <script>
 import axios from 'axios'
-const { VITE_API } = import.meta.env
+// const { VITE_API } = import.meta.env
 export default {
   data () {
     return {
-      url: VITE_API
+      // url: VITE_API
     }
   },
   methods: {
     logout () {
       axios
-        .post(`${this.url}/logout`)
+        .post(`${import.meta.env.VITE_API}/logout`)
         // 成功的結果
         .then((res) => {
           console.log(res)
           alert('登出成功')
+          document.cookie = 'hexToken=; expires=; path=/'
           this.$router.push('/')
         })
         // 失敗結果
         .catch((error) => {
-          console.dir(error) // 用dir可以展開資訊
+          console.dir(error.response.data.message) // 用dir可以展開資訊
         })
     }
   }
--- option/vite-template-standard/src/views/Admin/OrderView.vue
+++ option/vite-template-standard/src/views/Admin/OrderView.vue
@@ -1,9 +1,49 @@
 <template>
-  <h2>訂單</h2>
+  <div>
+    <h2>訂單</h2>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th>購買時間</th>
+          <th>Email</th>
+          <th>購買款項</th>
+          <th>應付金額</th>
+          <th>是否付款</th>
+          <th>編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="item in orders" :key="item.id">
+          <tr v-if="orders.length" :class="{ 'text-secondary': !item.is_paid }">
+            ...
+            <td>
+              <div class="btn-group">
+                <button
+                  class="btn btn-outline-primary btn-sm"
+                  type="button"
+                  @click="openModal(item)"
+                >
+                  檢視
+                </button>
+                <button
+                  class="btn btn-outline-danger btn-sm"
+                  type="button"
+                  @click="openDelOrderModal(item)"
+                >
+                  刪除
+                </button>
+              </div>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <!-- <OrderModal ref="orderModal" :order="tempOrder" @update-paid="updatePaid" /> -->
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style scoped></style>
