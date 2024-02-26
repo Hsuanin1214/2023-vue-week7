7cf8b1d07bcdf5ba89e10e516ab9913e0001bebb
Hsuan <welcome09210801@gmail.com>
2024-02-27 06:54:04 +0800

    feat(option api) 新增訂單頁面及modal

---
+++ option/vite-template-standard/src/components/OrderModal.vue
@@ -0,0 +1,26 @@
+// components/OrderModal.vue
+<template>
+  <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true" ref="modal">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>訂單細節</span>
+          </h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+            取消
+          </button>
+          <button type="button" class="btn btn-primary" @click="$emit('update-paid', tempOrder)">
+            修改付款狀態
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- option/vite-template-standard/src/views/Admin/OrderView.vue
+++ option/vite-template-standard/src/views/Admin/OrderView.vue
@@ -38,12 +38,58 @@
         </template>
       </tbody>
     </table>
-    <!-- <OrderModal ref="orderModal" :order="tempOrder" @update-paid="updatePaid" /> -->
+    <OrderModal ref="orderModal" :order="tempOrder" @update-paid="updatePaid" />
   </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+import OrderModal from '../../components/OrderModal.vue'
+
+const { VITE_API, VITE_PATH } = import.meta.env
+export default {
+  data () {
+    return {
+      orders: {},
+      tempOrder: {},
+      currentPage: 1,
+      url: VITE_API,
+      path: VITE_PATH
+    }
+  },
+  components: {
+    OrderModal
+  },
+  methods: {
+    openModal (item) {
+      this.tempOrder = { ...item }
+      const orderComponent = this.$refs.orderModal
+      orderComponent.openModal()
+    },
+    updatePaid (item) {
+      const api = `${this.url}/api/${this.path}/admin/order/${item.id}`
+      const paid = {
+        is_paid: item.is_paid
+      }
+      axios
+        .put(api, {
+          data: paid
+        })
+        // 成功的結果
+        .then((res) => {
+          console.log(res)
+          const orderComponent = this.$refs.orderModal
+          orderComponent.hideModal()
+          // this.getOrders(this.currentPage)
+        })
+        // 失敗結果
+        .catch((error) => {
+          console.log(error.response.data.message)
+          alert('未成功更新訂單資料')
+        })
+    }
+  }
+}
 </script>
 
 <style scoped></style>
