198441e25d5a8e0bd0a3d0dcaff81ece9ffee965
Hsuan <welcome09210801@gmail.com>
2024-02-29 07:21:36 +0800

    feat ( option api ) 渲染訂單列表

--- option/vite-template-standard/src/views/Admin/OrderView.vue
+++ option/vite-template-standard/src/views/Admin/OrderView.vue
@@ -13,33 +13,63 @@
         </tr>
       </thead>
       <tbody>
-        <template v-for="item in orders" :key="item.id">
-          <tr v-if="orders.length" :class="{ 'text-secondary': !item.is_paid }">
-            ...
-            <td>
-              <div class="btn-group">
-                <button
-                  class="btn btn-outline-primary btn-sm"
-                  type="button"
-                  @click="openModal(item)"
-                >
-                  檢視
-                </button>
-                <button
-                  class="btn btn-outline-danger btn-sm"
-                  type="button"
-                  @click="openDelOrderModal(item)"
-                >
-                  刪除
-                </button>
-              </div>
-            </td>
-          </tr>
+        <template v-for="item in orders" :key="item.id + 1">
+            <tr
+              v-if="orders.length"
+              :class="{ 'text-secondary': !item.is_paid }"
+            >
+              <!-- <td>{{ $filters.date(item.create_at) }}</td> -->
+              <td>{{ item.create_at }}</td>
+              <td><span v-text="item.user.email" v-if="item.user"></span></td>
+              <td>
+                <ul class="list-unstyled">
+                  <li v-for="product in item.products" :key="product.id">
+                    {{ product.product.title }} 數量：{{ product.qty }}
+                    {{ product.product.unit }}
+                  </li>
+                </ul>
+              </td>
+              <td class="text-right">{{ item.total }}</td>
+              <td>
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    :id="`paidSwitch${item.id}`"
+                    v-model="item.is_paid"
+                    @change="updatePaid(item)"
+                  />
+                  <label class="form-check-label" :for="`paidSwitch${item.id}`">
+                    <span v-if="item.is_paid">已付款</span>
+                    <span v-else>未付款</span>
+                  </label>
+                </div>
+              </td>
+              <td>
+                <div class="btn-group">
+                  <button
+                    class="btn btn-outline-primary btn-sm"
+                    type="button"
+                    @click="openModal(item)"
+                  >
+                    檢視
+                  </button>
+                  <button
+                    class="btn btn-outline-danger btn-sm"
+                    type="button"
+                    @click="openDelOrderModal(item)"
+                  >
+                    刪除
+                  </button>
+                </div>
+              </td>
+            </tr>
         </template>
       </tbody>
     </table>
+    <!-- 父元件的訂單資訊傳到子元件 -->
     <OrderModal ref="orderModal" :order="tempOrder" @update-paid="updatePaid" />
-    <!-- <DelModal ref="delModal" :item="tempOrder" @del-item="delOrder" /> -->
+    <DelModal ref="delModal" :item="tempOrder" @del-item="delOrder" />
   </div>
 </template>
 
@@ -66,33 +96,38 @@ export default {
   methods: {
     getOrders (currentPage = 1) {
       this.currentPage = currentPage
-      const url = `${this.url}/api/${this.path}/admin/orders?page=${currentPage}`
+      const url = `${this.url}api/${this.path}/admin/orders?page=${currentPage}`
       this.isLoading = true
-      axios.get(url, this.tempProduct).then((response) => {
-        this.orders = response.data.orders
-        this.pagination = response.data.pagination
-        // this.isLoading = false
-      }).catch((error) => {
-        // this.isLoading = false
-        // this.pushMessage({
-        //   style: 'danger',
-        //   title: '錯誤訊息',
-        //   content: error.response.data.message,
-        // })
-        alert(error)
-      })
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response)
+          this.orders = response.data.orders
+          this.pagination = response.data.pagination
+          // this.isLoading = false
+        })
+        .catch((error) => {
+          // this.isLoading = false
+          // this.pushMessage({
+          //   style: 'danger',
+          //   title: '錯誤訊息',
+          //   content: error.response.data.message,
+          // })
+          console.log(error.response.data.message)
+          alert(error)
+        })
     },
     openModal (item) {
       this.tempOrder = { ...item }
-      const orderComponent = this.$refs.orderModal
-      orderComponent.openModal()
+      const orderComponent = this.$refs.orderModal // 抓去子元件的orderModal
+      orderComponent.openModal() // orderModal函式開啟modal
     },
     openDelOrderModal (item) {
       this.tempOrder = { ...item }
       const delComponent = this.$refs.delModal
       delComponent.openModal()
     },
-    updatePaid (item) {
+    updatePaid (item) { // 子層傳回更改的訂單資訊或在父層這邊修正的is_paid狀態
       const api = `${this.url}/api/${this.path}/admin/order/${item.id}`
       const paid = {
         is_paid: item.is_paid
@@ -106,7 +141,7 @@ export default {
           console.log(res)
           const orderComponent = this.$refs.orderModal
           orderComponent.hideModal()
-          this.getOrders(this.currentPage)
+          this.getOrders(this.currentPage) // 重新渲染
         })
         // 失敗結果
         .catch((error) => {
