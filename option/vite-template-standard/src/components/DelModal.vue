24215717123808d0fb4201bb9d40188a8f84f0b2
Hsuan <welcome09210801@gmail.com>
2024-02-29 07:21:04 +0800

    feat ( option api ) 增加訂單介面

--- option/vite-template-standard/src/components/DelModal.vue
+++ option/vite-template-standard/src/components/DelModal.vue
@@ -10,7 +10,22 @@
   >
     <div class="modal-dialog" role="document">
       <div class="modal-content border-0">
-        ...
+        <div class="modal-header bg-danger text-white">
+          <h5 class="modal-title">
+            <span>刪除 {{ item.title }}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          是否刪除
+          <strong class="text-danger">{{ item.title }}</strong>
+          (刪除後將無法恢復)。
+        </div>
         <div class="modal-footer">
           <button
             type="button"
--- option/vite-template-standard/src/components/OrderModal.vue
+++ option/vite-template-standard/src/components/OrderModal.vue
@@ -9,13 +9,107 @@
           </h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body"></div>
-          ...
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md-4">
+              <h3>用戶資料</h3>
+              <table class="table">
+                <tbody v-if="tempOrder.user">
+                  <tr>
+                    <th style="width: 100px">姓名</th>
+                    <td>{{ tempOrder.user.name }}</td>
+                  </tr>
+                  <tr>
+                    <th>Email</th>
+                    <td>{{ tempOrder.user.email }}</td>
+                  </tr>
+                  <tr>
+                    <th>電話</th>
+                    <td>{{ tempOrder.user.tel }}</td>
+                  </tr>
+                  <tr>
+                    <th>地址</th>
+                    <td>{{ tempOrder.user.address }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-8">
+              <h3>訂單細節</h3>
+              <table class="table">
+                <tbody>
+                  <tr>
+                    <th style="width: 100px">訂單編號</th>
+                    <td>{{ tempOrder.id }}</td>
+                  </tr>
+                  <tr>
+                    <th>下單時間</th>
+                    <!-- <td>{{ $filters.date(tempOrder.create_at) }}</td> -->
+                    <td>{{ tempOrder.create_at }}</td>
+                  </tr>
+                  <tr>
+                    <th>付款時間</th>
+                    <td>
+                      <span v-if="tempOrder.paid_date">
+                        <!-- {{ $filters.date(tempOrder.paid_date) }} -->
+                        {{ tempOrder.paid_date }}
+                      </span>
+                      <span v-else>時間不正確</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>付款狀態</th>
+                    <td>
+                      <strong v-if="tempOrder.is_paid" class="text-success">已付款</strong>
+                      <span v-else class="text-muted">尚未付款</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>總金額</th>
+                    <td>
+                      <!-- {{ $filters.currency(tempOrder.total) }} -->
+                      {{ tempOrder.total }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <h3>選購商品</h3>
+              <table class="table">
+                <thead>
+                  <tr></tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in tempOrder.products" :key="item.id">
+                    <th>
+                      {{ item.product.title }}
+                    </th>
+                    <td>{{ item.qty }} / {{ item.product.unit }}</td>
+                    <td class="text-end">
+                      <!-- {{ $filters.currency(item.final_total) }} -->
+                      {{ item.final_total }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="d-flex justify-content-end">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                    v-model="tempOrder.is_paid" />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    <span v-if="tempOrder.is_paid">已付款</span>
+                    <span v-else>未付款</span>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
             取消
           </button>
-          <button type="button" class="btn btn-primary" @click="$emit('update-paid', tempOrder)">
+					<!-- 修改的資訊會再傳到父層 -->
+          <button type="button" class="btn btn-primary" @click="$emit('update-paid', tempOrder)"> 
             修改付款狀態
           </button>
         </div>
@@ -24,13 +118,12 @@
   </div>
 </template>
 
-// components/OrderModal.vue
 <script>
 import modalMixin from '@/mixins/modalMixin'
 
 export default {
   props: {
-    order: {
+    order: { //父層傳入單一訂單
       type: Object,
       default () {
         return {} // 每次傳都會生成新物件，避免不同組件的汙染
@@ -48,7 +141,7 @@ export default {
   mixins: [modalMixin], // 可以使用modalMixin的關閉功能
   watch: {
     order () {
-      this.tempOrder = this.order // 重新賦值避免動到父層的props
+      this.tempOrder = this.order // 重新賦值避免動到父層的資料
     }
   }
 }
