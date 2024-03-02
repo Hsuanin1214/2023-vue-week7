06dd2c36edb707a4f2c5242723f6068f4e937dab
Hsuan <welcome09210801@gmail.com>
2024-03-02 23:19:43 +0800

    feat ( option api ) 調整navbar寫法，建立navbar元件

---
+++ option/vite-template-standard/src/components/NavbarComponent.vue
@@ -0,0 +1,42 @@
+<template>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="https://imgur.com/a6LRmbN.jpg" alt="Bootstrap" width="30" height="24">
+          後台
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/product">產品管理</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/order">訂單管理</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/cupon">優惠券管理</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/post">貼文管理</RouterLink>
+            </li>
+            <li class="nav-item">
+              <a href="#" @click.prevent="logout" class="nav-link">登出</a>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/blog">Blog</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/admin/cart">購物車</RouterLink>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+</template>
--- option/vite-template-standard/src/views/Admin/AdminView.vue
+++ option/vite-template-standard/src/views/Admin/AdminView.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="home">
-    <h1>後台</h1>
-    <!-- <div class="collapse navbar-collapse"> -->
-      <div class="navbar-nav">
-        <RouterLink to="/admin">後台首頁</RouterLink> |
-        <RouterLink to="/admin/product">產品管理</RouterLink> |
-        <RouterLink to="/admin/order">訂單管理</RouterLink> |
-        <RouterLink to="/admin/cupon">優惠券管理</RouterLink> |
-        <RouterLink to="/admin/post">貼文管理</RouterLink>
-        <a href="#" @click.prevent="logout" class="nav-link">登出</a>
-      </div>
-      <div class="navbar-nav ms-auto">
-        <RouterLink to="/admin/blog">Blog</RouterLink> |
-        <RouterLink to="/admin/cart">購物車</RouterLink>
-      </div>
-    </div>
-  <!-- </div> -->
-  <hr />
+    <NavbarComponent/>
   <RouterView></RouterView>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import NavbarComponent from '../../components/NavbarComponent.vue'
 const { VITE_API } = import.meta.env
 export default {
+  components: { NavbarComponent },
   data () {
     return {
       url: VITE_API
@@ -48,8 +36,7 @@ export default {
     },
     logout () {
       axios
-        .post(`${this.url}/logout`)
-        // 成功的結果
+        .post(`${this.url}/logout`)// 成功的結果
         .then((res) => {
           console.log(res)
           alert('登出成功')
