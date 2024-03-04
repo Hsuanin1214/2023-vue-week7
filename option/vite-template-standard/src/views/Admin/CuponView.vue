fde3bfec66c6c41fa7ca8365bac2dac357d609e8
Hsuan <welcome09210801@gmail.com>
2024-03-04 23:02:13 +0800

    style ( option api ) 優惠券前後台切版

--- option/vite-template-standard/src/views/Admin/CuponView.vue
+++ option/vite-template-standard/src/views/Admin/CuponView.vue
@@ -1,26 +1,83 @@
 <template>
   <div>
-    <div>
+    <div class="mb-3">
       <h2>優惠券管理</h2>
       <span>目前共有兩張優惠券</span>
     </div>
     <div>
-      <button type="button">新增優惠券</button>
+      <button type="button" class="btn btn-outline-primary mb-3">新增優惠券</button>
     </div>
-    <div class="box">
-      <div class="coupon">
-        <div class="d-block">
-          <p>10% 優惠券</p>
-          <p>到期日 : 2025/10/15</p>
+    <div class="row">
+      <div class="list-item-container col-5">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="list-group-icon">
+            </span>
+            List item
+            <span class="arrow">&rsaquo;</span>
+          </li>
+        </ul>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="list-group-icon">
+            </span>
+            List item
+            <span class="arrow">&rsaquo;</span>
+          </li>
+        </ul>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="list-group-icon">
+            </span>
+            List item
+            <span class="arrow">&rsaquo;</span>
+          </li>
+        </ul>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="list-group-icon">
+            </span>
+            List item
+            <span class="arrow">&rsaquo;</span>
+          </li>
+        </ul>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <span class="list-group-icon">
+            </span>
+            List item
+            <span class="arrow">&rsaquo;</span>
+          </li>
+        </ul>
+      </div>
+      <div ref='couponModal' class="col-7">
+        <div>
+          <div class="mb-3 form-switch">
+            <label class="form-check-label me-3" for="test">狀態</label>
+            <input class="form-check-input ms-5" type="checkbox" role="switch" id="test">
+          </div>
+          <div class="mb-3">
+            <label for="couponName" class="form-label is-required">優惠券名稱</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="couponName" class="form-label is-required">優惠碼</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="couponName" class="col-3">折扣</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="couponName" class="col-3">有效日期</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="d-flex">
+          <button type="button" class="btn btn-outline-primary me-2">編輯</button>
+          <button type="button" class="btn btn-outline-primary">刪除</button>
         </div>
       </div>
-      <div class="flex">
-        <button type="button">編輯</button>
-        <button type="button">刪除</button>
-      </div>
-    </div>
-    <div class="box">
-      <div class="coupon"><p>15% 優惠券</p></div>
     </div>
   </div>
 </template>
@@ -30,49 +87,44 @@ export default {}
 </script>
 
 <style scoped>
-.box{
-  position: relative;
+.form-switch{
+  padding-left: 0;
 }
-.coupon{
-    width: 400px;
-    height: 120px;
-    margin: 40px auto;
-    color: #fff;
-    position: relative;
-    background-color: transparent;
-    background-image:
-    radial-gradient(circle at 2px 8px, transparent 4px, #fff 4px, #ACC 0px),radial-gradient(circle at 82px 8px, transparent 4px, #fff 4px, #acc 0px);
-    background-size: 316px 20px;
-    background-repeat: no-repeat repeat;
-    background-position: 0px 2px,316px 2px;
+.list-item-container {
+  max-width: 300px;
+  /* margin: auto; */
 }
-.coupon p{
-  font-size: 24px;
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 40px;
-  transform: translate(0,-50%);
+
+.list-group-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
 }
-.coupon::before{
-    content: "";
-    display: block;
-    position: absolute;
-    height: 100%;
-    left: 60%;
-    top: 0;
-    border-left: 2px dotted #fff;
+
+.list-group-item .list-group-icon {
+  background-color: #e9ecef; /* This is Bootstrap's $gray-200 */
+  border-radius: 50%;
+  padding: 10px;
+  margin-right: 10px;
 }
-.coupon::after{
-  content: "立 即 领 取";
-  font-size: 16px;
-  display: block;
-  width: 50px;
-  position: absolute;
-  top: 50%;
-  right: 4rem;
-  transform: translate(-50%,-50%);
-  text-align: center;
-  white-space: pre-line;
+
+.list-group-item .list-group-icon {
+  width: 35px; /* Specify width for the icon */
+  height: 35px; /* Specify height for the icon */
+  background-color: #e9ecef; /* This is Bootstrap's $gray-200 */
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.list-group-item .list-group-icon::before {
+  content: '15%'; /* FontAwesome check icon - ensure FontAwesome is included if you use this */
+  font-family: 'FontAwesome'; /* Specify FontAwesome as the font family */
+  font-size: 16px; /* Adjust icon size */
+}
+
+.list-group-item .arrow {
+  font-size: 1.5em; /* Adjust the size of the arrow */
 }
 </style>
---
+++ option/vite-template-standard/src/views/Front/CuponView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div>
+      <h2>優惠券管理</h2>
+      <span>目前共有兩張優惠券</span>
+    </div>
+    <div>
+      <button type="button">新增優惠券</button>
+    </div>
+    <div class="box d-flex">
+      <div class="coupon">
+        <div>
+          <p>10% 優惠券</p>
+          <p>到期日 : 2025/10/15</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.box{
+  position: relative;
+}
+.coupon{
+    width: 400px;
+    height: 120px;
+    margin: 40px auto;
+    color: #fff;
+    position: relative;
+    background-color: transparent;
+    background-image:
+    radial-gradient(circle at 2px 8px, transparent 4px, #fff 4px, #ACC 0px),radial-gradient(circle at 82px 8px, transparent 4px, #fff 4px, #acc 0px);
+    background-size: 316px 20px;
+    background-repeat: no-repeat repeat;
+    background-position: 0px 2px,316px 2px;
+}
+.coupon p{
+  font-size: 24px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 40px;
+  transform: translate(0,-50%);
+}
+.coupon::before{
+    content: "";
+    display: block;
+    position: absolute;
+    height: 100%;
+    left: 60%;
+    top: 0;
+    border-left: 2px dotted #fff;
+}
+.coupon::after{
+  content: "立 即 领 取";
+  font-size: 16px;
+  display: block;
+  width: 50px;
+  position: absolute;
+  top: 50%;
+  right: 4rem;
+  transform: translate(-50%,-50%);
+  text-align: center;
+  white-space: pre-line;
+}
+</style>
