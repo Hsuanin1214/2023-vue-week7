7b13be5033169613cab26ba15b7f443ffc439964
Hsuan <welcome09210801@gmail.com>
2024-03-04 23:19:39 +0800

    feat ( option api ) 新增優惠券驗證

--- option/vite-template-standard/src/views/Admin/CuponView.vue
+++ option/vite-template-standard/src/views/Admin/CuponView.vue
@@ -11,73 +11,97 @@
       <div class="list-item-container col-5">
         <ul class="list-group">
           <li class="list-group-item">
-            <span class="list-group-icon">
-            </span>
+            <span class="badge text-bg-secondary">New</span>
             List item
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
         <ul class="list-group">
           <li class="list-group-item">
-            <span class="list-group-icon">
-            </span>
+            <span class="badge text-bg-secondary">New</span>
             List item
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
         <ul class="list-group">
           <li class="list-group-item">
-            <span class="list-group-icon">
-            </span>
+            <span class="badge text-bg-secondary">New</span>
             List item
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
         <ul class="list-group">
           <li class="list-group-item">
-            <span class="list-group-icon">
-            </span>
+            <span class="badge text-bg-secondary">New</span>
             List item
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
         <ul class="list-group">
           <li class="list-group-item">
-            <span class="list-group-icon">
-            </span>
+            <span class="badge text-bg-secondary">New</span>
             List item
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
       </div>
-      <div ref='couponModal' class="col-7">
-        <div>
-          <div class="mb-3 form-switch">
-            <label class="form-check-label me-3" for="test">狀態</label>
-            <input class="form-check-input ms-5" type="checkbox" role="switch" id="test">
+      <v-form class="col-7" v-slot="{meta,errors}" @submit="confirmUpdate" ref="form">
+        <!-- <div ref='couponModal' class="col-7"> -->
+          <div>
+            <div class="mb-3 form-switch">
+              <label class="form-check-label me-3" for="test">是否開啟</label>
+              <input class="form-check-input ms-5" type="checkbox" role="switch" id="test">
+            </div>
+            <div class="mb-3">
+              <label for="couponName" class="form-label is-required">優惠券名稱</label>
+              <v-field
+                id="couponName"
+                name="優惠券名稱"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors['優惠券名稱'] }"
+                placeholder="請輸入優惠券名稱"
+                rules="required"
+              ></v-field>
+              <error-message name="優惠券名稱" class="invalid-feedback"></error-message>
+            </div>
+            <div class="mb-3">
+              <label for="couponNum" class="form-label is-required">優惠碼</label>
+              <v-field
+                id="couponNum"
+                name="優惠碼"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors['優惠碼'] }"
+                placeholder="請輸入優惠碼"
+                rules="required"
+              ></v-field>
+              <error-message name="優惠碼" class="invalid-feedback"></error-message>
+            </div>
+            <div class="mb-3">
+              <label for="couponDiscount" class="col-3">折扣</label>
+              <v-field
+                id="couponDiscount"
+                name="折扣"
+                type="number"
+                class="form-control"
+                :class="{ 'is-invalid': errors['折扣'] }"
+                placeholder="請輸入折扣"
+                rules="required|integer|min_value:1|max_value:100"
+              ></v-field>
+              <error-message name="折扣" class="invalid-feedback"></error-message>
+            </div>
+            <div class="mb-3">
+              <label for="couponName" class="col-3">有效日期</label>
+              <input type="date" class="form-control">
+            </div>
           </div>
-          <div class="mb-3">
-            <label for="couponName" class="form-label is-required">優惠券名稱</label>
-            <input type="text" class="form-control">
+          <div class="d-flex">
+            <button type="button" class="btn btn-outline-primary me-2" :disabled="!meta.valid">確認</button>
+            <button type="button" class="btn btn-outline-primary">刪除</button>
           </div>
-          <div class="mb-3">
-            <label for="couponName" class="form-label is-required">優惠碼</label>
-            <input type="text" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label for="couponName" class="col-3">折扣</label>
-            <input type="text" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label for="couponName" class="col-3">有效日期</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="d-flex">
-          <button type="button" class="btn btn-outline-primary me-2">編輯</button>
-          <button type="button" class="btn btn-outline-primary">刪除</button>
-        </div>
-      </div>
+        <!-- </div> -->
+      </v-form>
     </div>
   </div>
 </template>
