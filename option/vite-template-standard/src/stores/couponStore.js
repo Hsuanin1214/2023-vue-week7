325ad6bd60f59dc3cadda1cad5b101bb7687ffc9
ERI\welcome09210801 <welcome09210801@eri.com.tw>
2024-03-05 17:25:01 +0800

    feat ( option api ) 新增優惠券pinia、渲染及新增更新刪除

---
+++ option/vite-template-standard/src/stores/couponStore.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+//
+import { defineStore } from 'pinia'
+
+const { VITE_API, VITE_PATH } = import.meta.env
+
+export default defineStore('couponStore', {
+  state: () => (
+    {
+      coupons: [],
+      page: {}
+    }),
+  actions: {
+    getCoupons (page = 1) {
+      // 給參數預設值
+      const getUrl = `${VITE_API}/api/${VITE_PATH}/admin/coupons?page=${page}` // (query)為網址參數寫法，page參數帶入，取得當前頁碼的產品資料
+      axios
+        .get(getUrl)
+        .then((res) => {
+          const { coupons, pagination } = res.data
+          this.coupons = coupons
+          this.page = pagination
+          this.isLoading = !this.isLoading
+        })
+        .catch((error) => {
+          alert(error.response.data.message)
+          console.log(error)
+        })
+    },
+    updateCoupons (status, data) {
+      console.log(status, data)
+      let updateOrNewUrl = `${VITE_API}/api/${VITE_PATH}/admin/coupon/${data.id}`
+      let http = 'put'
+      if (status === 'new') {
+        updateOrNewUrl = `${VITE_API}/api/${VITE_PATH}/admin/coupon`
+        http = 'post'
+      }
+      console.log(updateOrNewUrl, http)
+      axios[http](updateOrNewUrl, { data })
+        .then((res) => {
+          alert(res.data.message)
+          this.getCoupons() // 取得所有產品
+        })
+        .catch((error) => {
+          alert(error.response.data.message)
+        })
+    },
+    switchStatus (event, data) {
+      const current = event.target.checked ? 1 : 0
+      const newData = { ...data, is_enabled: current }
+      // loaderStore.createLoader('switch-coupon-status')
+      axios.put(`${VITE_API}/api/${VITE_PATH}/admin/coupon/${data.id}`, { data: newData })
+        .then(() => {
+          // alertStore.toastAlert(`成功${current ? '取消' : '開啟'}優惠券`, 'success');
+          this.getCoupons()
+        })
+        .catch((error) => alert(error.response.data.message))
+      // .finally(() => loaderStore.removeLoader('switch-coupon-status'))
+    }
+  }
+})
--- option/vite-template-standard/src/views/Admin/CuponView.vue
+++ option/vite-template-standard/src/views/Admin/CuponView.vue
@@ -2,103 +2,88 @@
   <div>
     <div class="mb-3">
       <h2>優惠券管理</h2>
-      <span>目前共有兩張優惠券</span>
+      <span>目前共有<span class="fs-4 text-success fw-bold mx-2">{{coupons.length}}</span>張優惠券</span>
     </div>
     <div>
-      <button type="button" class="btn btn-outline-primary mb-3">新增優惠券</button>
+      <button type="button" class="btn btn-outline-primary mb-3" @click="openCoupon(coupon)">新增優惠券</button>
     </div>
     <div class="row">
       <div class="list-item-container col-5">
-        <ul class="list-group">
-          <li class="list-group-item">
-            <span class="badge text-bg-secondary">New</span>
-            List item
+        <ul class="list-group" v-for="coupon in coupons" :key="coupon.id + 1">
+          <li class="list-group-item" @click="openCoupon(coupon)">
+            <span class="badge text-bg-secondary">{{coupon.is_enabled == 1 ? '開啟' : '關閉'}}</span>
+            {{coupon.title}}
             <span class="arrow">&rsaquo;</span>
           </li>
         </ul>
-        <ul class="list-group">
-          <li class="list-group-item">
-            <span class="badge text-bg-secondary">New</span>
-            List item
-            <span class="arrow">&rsaquo;</span>
-          </li>
-        </ul>
-        <ul class="list-group">
-          <li class="list-group-item">
-            <span class="badge text-bg-secondary">New</span>
-            List item
-            <span class="arrow">&rsaquo;</span>
-          </li>
-        </ul>
-        <ul class="list-group">
-          <li class="list-group-item">
-            <span class="badge text-bg-secondary">New</span>
-            List item
-            <span class="arrow">&rsaquo;</span>
-          </li>
-        </ul>
-        <ul class="list-group">
-          <li class="list-group-item">
-            <span class="badge text-bg-secondary">New</span>
-            List item
-            <span class="arrow">&rsaquo;</span>
-          </li>
-        </ul>
+        <!-- 分頁元件 -->
+        <!-- <pagination-component :pages="pagination" :get-products="getCoupons"></pagination-component> -->
       </div>
       <v-form class="col-7" v-slot="{meta,errors}" @submit="confirmUpdate" ref="form">
         <!-- <div ref='couponModal' class="col-7"> -->
-          <div>
-            <div class="mb-3 form-switch">
-              <label class="form-check-label me-3" for="test">是否開啟</label>
-              <input class="form-check-input ms-5" type="checkbox" role="switch" id="test">
+          <div v-if="isNoClick" class="d-flex justify-content-center align-items-center">目前未選擇任何優惠券</div>
+          <div v-else>
+            <div>
+              <div class="mb-3 form-switch">
+                <label class="form-check-label me-3" for="test">是否開啟</label>
+                <input class="form-check-input ms-5" type="checkbox"
+                  role="switch"
+                  id="test"
+                  :checked="tempCoupon.is_enabled"
+                  @change="switchStatus($event, tempCoupon)"
+                >
+              </div>
+              <div class="mb-3">
+                <label for="couponName" class="form-label is-required">優惠券名稱</label>
+                <v-field
+                  id="couponName"
+                  name="優惠券名稱"
+                  type="text"
+                  class="form-control"
+                  :class="{ 'is-invalid': errors['優惠券名稱'] }"
+                  placeholder="請輸入優惠券名稱"
+                  rules="required"
+                  v-model="tempCoupon.title"
+                ></v-field>
+                <error-message name="優惠券名稱" class="invalid-feedback"></error-message>
+              </div>
+              <div class="mb-3">
+                <label for="couponNum" class="form-label is-required">優惠碼</label>
+                <v-field
+                  id="couponNum"
+                  name="優惠碼"
+                  type="text"
+                  class="form-control"
+                  :class="{ 'is-invalid': errors['優惠碼'] }"
+                  placeholder="請輸入優惠碼"
+                  rules="required"
+                  v-model="tempCoupon.code"
+                ></v-field>
+                <error-message name="優惠碼" class="invalid-feedback"></error-message>
+              </div>
+              <div class="mb-3">
+                <label for="couponDiscount" class="col-3">折扣</label>
+                <v-field
+                  id="couponDiscount"
+                  name="折扣"
+                  type="number"
+                  class="form-control"
+                  :class="{ 'is-invalid': errors['折扣'] }"
+                  placeholder="請輸入折扣"
+                  rules="required|integer|min_value:1|max_value:100"
+                  v-model="tempCoupon.percent"
+                ></v-field>
+                <error-message name="折扣" class="invalid-feedback"></error-message>
+              </div>
+              <div class="mb-3">
+                <label for="couponName" class="col-3">有效日期</label>
+                <input id="due_date" type="date" class="form-control" v-model="due_date">
+              </div>
             </div>
-            <div class="mb-3">
-              <label for="couponName" class="form-label is-required">優惠券名稱</label>
-              <v-field
-                id="couponName"
-                name="優惠券名稱"
-                type="text"
-                class="form-control"
-                :class="{ 'is-invalid': errors['優惠券名稱'] }"
-                placeholder="請輸入優惠券名稱"
-                rules="required"
-              ></v-field>
-              <error-message name="優惠券名稱" class="invalid-feedback"></error-message>
+            <div class="d-flex">
+              <button type="submit" class="btn btn-outline-primary me-2" :disabled="!meta.valid">確認</button>
+              <button type="button" class="btn btn-outline-primary">刪除</button>
             </div>
-            <div class="mb-3">
-              <label for="couponNum" class="form-label is-required">優惠碼</label>
-              <v-field
-                id="couponNum"
-                name="優惠碼"
-                type="text"
-                class="form-control"
-                :class="{ 'is-invalid': errors['優惠碼'] }"
-                placeholder="請輸入優惠碼"
-                rules="required"
-              ></v-field>
-              <error-message name="優惠碼" class="invalid-feedback"></error-message>
-            </div>
-            <div class="mb-3">
-              <label for="couponDiscount" class="col-3">折扣</label>
-              <v-field
-                id="couponDiscount"
-                name="折扣"
-                type="number"
-                class="form-control"
-                :class="{ 'is-invalid': errors['折扣'] }"
-                placeholder="請輸入折扣"
-                rules="required|integer|min_value:1|max_value:100"
-              ></v-field>
-              <error-message name="折扣" class="invalid-feedback"></error-message>
-            </div>
-            <div class="mb-3">
-              <label for="couponName" class="col-3">有效日期</label>
-              <input type="date" class="form-control">
-            </div>
-          </div>
-          <div class="d-flex">
-            <button type="button" class="btn btn-outline-primary me-2" :disabled="!meta.valid">確認</button>
-            <button type="button" class="btn btn-outline-primary">刪除</button>
           </div>
         <!-- </div> -->
       </v-form>
@@ -107,7 +92,63 @@
 </template>
 
 <script>
-export default {}
+import { mapActions, mapState } from 'pinia'
+import couponStore from '../../stores/couponStore.js'
+// import PaginationComponent from '../../components/PaginationComponent.vue'
+export default {
+  // components: {
+  //   PaginationComponent
+  // },
+  data () {
+    return {
+      tempCoupon: {},
+      status: '',
+      isNoClick: true
+    }
+  },
+  computed: {
+    ...mapState(couponStore, ['coupons', 'page']),
+    due_date: {
+      get () {
+        const date = new Date(this.tempCoupon.due_date * 1000)
+        const year = date.getFullYear()
+        const month = ('0' + (date.getMonth() + 1)).slice(-2)
+        const day = ('0' + date.getDate()).slice(-2)
+        return `${year}-${month}-${day}`
+      },
+      set (date) {
+        this.tempCoupon.due_date = new Date(date).getTime() / 1000
+      }
+    }
+  },
+  methods: {
+    ...mapActions(couponStore, ['getCoupons', 'switchStatus', 'updateCoupons']),
+    openCoupon (item) {
+      this.isNoClick = false
+      this.tempCoupon = item
+        ? { ...item }
+        : {
+            is_enabled: 0
+          }
+      this.status = item
+        ? 'edit'
+        : 'new'
+    },
+    confirmUpdate () {
+      console.log(this.status)
+      if (this.tempCoupon.id) {
+        console.log('edit')
+        this.updateCoupons('edit', this.tempCoupon)
+      } else {
+        console.log('new')
+        this.updateCoupons('new', this.tempCoupon)
+      }
+    }
+  },
+  mounted () {
+    this.getCoupons()
+  }
+}
 </script>
 
 <style scoped>
