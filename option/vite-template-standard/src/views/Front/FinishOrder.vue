30a5817adf68232ffa98478493c9d1c08f622439
Hsuan <welcome09210801@gmail.com>
2024-03-21 07:53:53 +0800

    fix ( option api ) 調整完成訂單按鈕路由

--- option/vite-template-standard/src/views/Front/FinishOrder.vue
+++ option/vite-template-standard/src/views/Front/FinishOrder.vue
@@ -30,6 +30,7 @@
     </div>
     <p>嗜甜感謝您的信任，請等待我們用心製作的甜點，祝您有個愉快的一天。</p>
     <p>若有商品相關問題，請洽本店服務人員。</p>
-    <router-link class="btn btn-outline-primary py-3 px-7 my-3" to="/">返回首頁</router-link>
+    <router-link class="btn btn-info py-3 px-4 my-3 me-3" to="/">返回首頁</router-link>
+    <router-link class="btn btn-warning py-3 px-4 my-3" to="/orders">查看訂單</router-link>
   </div>
 </template>
